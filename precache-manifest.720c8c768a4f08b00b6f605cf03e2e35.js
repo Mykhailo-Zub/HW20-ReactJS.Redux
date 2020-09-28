460d8235e8717893189ac112d2fc448695a4a62b
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-09-28 13:28:26 +0300

    Updates

---
+++ precache-manifest.720c8c768a4f08b00b6f605cf03e2e35.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d019a2b2b3060698f6c67d419d56c433",
+    "url": "/HW20-ReactJS.Redux/index.html"
+  },
+  {
+    "revision": "8217cc72a6b803155413",
+    "url": "/HW20-ReactJS.Redux/static/css/main.7fca0e32.chunk.css"
+  },
+  {
+    "revision": "05c59fc4c720df55fd38",
+    "url": "/HW20-ReactJS.Redux/static/js/2.5e01e8e7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HW20-ReactJS.Redux/static/js/2.5e01e8e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8217cc72a6b803155413",
+    "url": "/HW20-ReactJS.Redux/static/js/main.32471666.chunk.js"
+  },
+  {
+    "revision": "9f1db73a280127693d33",
+    "url": "/HW20-ReactJS.Redux/static/js/runtime-main.5be93c7d.js"
+  },
+  {
+    "revision": "0002e5f6fa980eed02d3897ecc5eb36d",
+    "url": "/HW20-ReactJS.Redux/static/media/comment.0002e5f6.svg"
+  },
+  {
+    "revision": "e371e0b38f5eb7a65737f412a81f0db4",
+    "url": "/HW20-ReactJS.Redux/static/media/comment_active.e371e0b3.svg"
+  },
+  {
+    "revision": "39726703564a2a71e8077948ac5d964c",
+    "url": "/HW20-ReactJS.Redux/static/media/confirm.39726703.svg"
+  },
+  {
+    "revision": "da3338122f87907c1fb994f17002f866",
+    "url": "/HW20-ReactJS.Redux/static/media/down-arrow.da333812.svg"
+  },
+  {
+    "revision": "12220a2259c8f1718847197c4bfdb84e",
+    "url": "/HW20-ReactJS.Redux/static/media/like.12220a22.svg"
+  },
+  {
+    "revision": "f8fb9825e43cd716ad637aa42aae3994",
+    "url": "/HW20-ReactJS.Redux/static/media/like_active.f8fb9825.svg"
+  },
+  {
+    "revision": "444db2463624b56ae55674c6e654d9b9",
+    "url": "/HW20-ReactJS.Redux/static/media/share.444db246.svg"
+  },
+  {
+    "revision": "abbcc3bb79d684270cdfb4d788fc558d",
+    "url": "/HW20-ReactJS.Redux/static/media/share_active.abbcc3bb.svg"
+  },
+  {
+    "revision": "8f047106898ba92d2dc7e03f29f16bed",
+    "url": "/HW20-ReactJS.Redux/static/media/upload.8f047106.svg"
+  }
+]);
